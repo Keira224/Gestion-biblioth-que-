ab39695e26edc5799bf3c3142db92ef73b217472
Keira <ousmanekeira224@gmail.com>
2025-12-31 22:39:03 +0000

    Version qui permet de telecharger un e-book

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,8 @@ urlpatterns = [
     path('', include('exemplaires.urls')),
     path('', include('ouvrages.urls')),
     path('', include('core.urls')),
+    
 ]
+
+# Pour Activer l’accès aux fichiers PDF en local
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
